5863cfc31b8b2a24d97ca8972ec3b7956f015dd6
AndreVTavares <andretavares3@gmail.com>
2021-01-18 16:50:43 -0300

    First commit migrations models and some services and controllers

--- src/modules/products/infra/typeorm/repositories/ProductsRepository.ts
+++ src/modules/products/infra/typeorm/repositories/ProductsRepository.ts
@@ -22,20 +22,62 @@ class ProductsRepository implements IProductsRepository {
     quantity,
   }: ICreateProductDTO): Promise<Product> {
     // TODO
+    const product = this.ormRepository.create({
+      name,
+      price,
+      quantity,
+    });
+
+    await this.ormRepository.save(product);
+
+    return product;
   }
 
   public async findByName(name: string): Promise<Product | undefined> {
     // TODO
+
+    const product = this.ormRepository.findOne({
+      where: { name },
+    });
+
+    return product;
   }
 
   public async findAllById(products: IFindProducts[]): Promise<Product[]> {
     // TODO
+    const productIds = products.map(product => product.id);
+
+    const existentproducts = await this.ormRepository.find({
+      where: {
+        id: In(productIds),
+      },
+    });
+
+    return existentproducts;
   }
 
   public async updateQuantity(
     products: IUpdateProductsQuantityDTO[],
   ): Promise<Product[]> {
     // TODO
+    const items = await this.ormRepository.findByIds(products.map(p => p.id));
+
+    return Promise.all(
+      items.map(async item => {
+        const product = products.find(p => p.id === item.id);
+
+        if (product) {
+          const p = item;
+          p.quantity = item.quantity - product.quantity;
+
+          await this.ormRepository.save(p);
+
+          return p;
+        }
+
+        return item;
+      }),
+    );
   }
 }
 
